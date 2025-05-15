c003405d8fe907a5da195508fbadc530cd84f9a4
agorbar <agorbar65@gmail.com>
2025-05-15 15:20:37 +0300

    Itse visuaalinen tietovisalaatikko

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ body {
   align-items: center;
   height: 100vh;
   padding: 10px;
+}
+#quiz-container {
+  background: #1a1a2e;
+  border-radius: 15px;
+  width: 350px;
+  max-height: 600px;
+  padding: 25px 30px 30px 30px;
+  box-shadow: 0 8px 25px rgba(0,0,0,0.3);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
